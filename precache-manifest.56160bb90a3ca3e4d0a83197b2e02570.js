2d9d40d967ac034e7eb5d38fbb531b229486f8d3
Matheus Martino <matheus.martino97@gmail.com>
2021-04-10 16:56:36 -0300

    Updates

---
+++ precache-manifest.56160bb90a3ca3e4d0a83197b2e02570.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5430904ecf48394add2c5ee20809c320",
+    "url": "/20_sd-09-project-trivia-react-redux/index.html"
+  },
+  {
+    "revision": "3d5c15391e17a4b8a224",
+    "url": "/20_sd-09-project-trivia-react-redux/static/css/main.e11ec6ac.chunk.css"
+  },
+  {
+    "revision": "3830b672c8b99967942f",
+    "url": "/20_sd-09-project-trivia-react-redux/static/js/2.38f00c61.chunk.js"
+  },
+  {
+    "revision": "df51dc662a3fbdda799515cda1d42e2e",
+    "url": "/20_sd-09-project-trivia-react-redux/static/js/2.38f00c61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d5c15391e17a4b8a224",
+    "url": "/20_sd-09-project-trivia-react-redux/static/js/main.464e595b.chunk.js"
+  },
+  {
+    "revision": "9a6666ccf1de5e639899",
+    "url": "/20_sd-09-project-trivia-react-redux/static/js/runtime-main.e79d4921.js"
+  }
+]);
