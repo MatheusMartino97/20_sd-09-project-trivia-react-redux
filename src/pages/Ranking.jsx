9e5a65cb4fee60363192b731215e109f75534cfd
Matheus Martino <matheus.martino97@gmail.com>
2021-04-14 16:36:37 -0300

    Adding style to the ranking page.

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -14,12 +14,16 @@ class Ranking extends Component {
       //   return acc;
       // }, [])
       .map((player, index) => (
-        <li key={ index }>
+        <li key={ index } className="player">
+          <p>{`${index + 1}°`}</p>
+          <img src={ player.picture } alt="Gravatar" />
           <ul>
             <li data-testid={ `player-name-${index}` }>
+              <strong>Jogador: </strong>
               {player.name}
             </li>
             <li data-testid={ `player-score-${index}` }>
+              <strong>Pontuação: </strong>
               {player.score}
             </li>
           </ul>
@@ -27,15 +31,17 @@ class Ranking extends Component {
       ));
 
     return (
-      <>
-        <h1 data-testid="ranking-title">Ranking</h1>
+      <div id="ranking-page">
+        <div className="title">
+          <h1 data-testid="ranking-title">Ranking</h1>
+          <Link to="/">
+            <button type="button" data-testid="btn-go-home">Home</button>
+          </Link>
+        </div>
         <ol>
           { orderedRanking }
         </ol>
-        <Link to="/">
-          <button type="button" data-testid="btn-go-home">Home</button>
-        </Link>
-      </>
+      </div>
     );
   }
 }
