00dd1a4cfecca04ee7bc9120b233eb7f4c7c7deb
Matheus Martino <matheus.martino97@gmail.com>
2021-04-14 15:58:52 -0300

    Adding style to feedback page.

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import md5 from 'crypto-js/md5';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import emoji from 'emoji-dictionary';
 
 class Feedback extends Component {
   componentDidMount() {
@@ -24,24 +25,16 @@ class Feedback extends Component {
     const cryptoEmail = md5(email).toString();
     const { player } = JSON.parse(localStorage.getItem('state'));
     const minimumAssertions = 3;
-
+    const wellDone = <p>{`Mandou bem! ${emoji.getUnicode('smile')}`}</p>;
+    const couldBeBetter = <p>{`Podia ser melhor... ${emoji.getUnicode('grimacing')}`}</p>;
     return (
-      <>
+      <div id="feedback-page">
         <header>
           <img data-testid="header-profile-picture" src={ `https://www.gravatar.com/avatar/${cryptoEmail}` } alt="profile" />
           <h1 data-testid="header-player-name">{ player.name }</h1>
-          <p data-testid="header-score">{ player.score }</p>
+          {/* <p data-testid="header-score">{ player.score }</p> */}
         </header>
         <main>
-          <section>
-            <p data-testid="feedback-text">
-              {
-                player.assertions >= minimumAssertions
-                  ? 'Mandou bem!'
-                  : 'Podia ser melhor...'
-              }
-            </p>
-          </section>
           <section>
             <p>
               Pontuação:&nbsp;
@@ -57,6 +50,15 @@ class Feedback extends Component {
             </p>
           </section>
           <section>
+            <p data-testid="feedback-text">
+              {
+                player.assertions >= minimumAssertions
+                  ? wellDone
+                  : couldBeBetter
+              }
+            </p>
+          </section>
+          <section>
             <Link to="/">
               <button data-testid="btn-play-again" type="button">Jogar novamente</button>
             </Link>
@@ -65,7 +67,7 @@ class Feedback extends Component {
             </Link>
           </section>
         </main>
-      </>
+      </div>
     );
   }
 }
